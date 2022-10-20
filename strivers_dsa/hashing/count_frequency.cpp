c6d448c148d885c429008e7a0a7c1ed3a12a3f33
coolabhays <abhaysp9955@gmail.com>
2022-10-20 11:31:25 +0530

    feat: basic count frequency (hashing)

---
+++ strivers_dsa/hashing/count_frequency.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+#define INF 1e6
+
+std::unordered_map<int, int> count_frequency(const int* const arr, size_t sz) {
+	std::unordered_map<int, int> umap{};
+	for (size_t i = 0; i < sz; i++) {
+		umap[arr[i]]++;
+	}
+
+	return umap;
+}
+
+// for approach with O(n), there are two ways, use new_arr[arr[i]]++ or use two loops
+
+
+int main(void) {
+	int arr[(size_t)INF] = {'\0'};
+	size_t count{};
+	while (std::cin >> arr[count++]) { }
+	std::unordered_map<int, int> res = count_frequency(arr, count -1);
+	for (const std::pair<int, int>& x: res) {
+		std::cout << x.first << ", " << x.second << '\n';
+	}
+	return 0;
+}
