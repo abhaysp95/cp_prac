2fa4c7977fee797b507af113d7eb43834266922c
coolabhays <abhaysp9955@gmail.com>
2022-10-20 15:07:12 +0530

    feat: find second largest and second smallest in array
        another approach (still no sorting)

--- strivers_dsa/array/second_largest_without_sorting.cpp
+++ strivers_dsa/array/second_largest_without_sorting.cpp
@@ -35,12 +35,37 @@ int second_smallest(const int* const arr, size_t sz) {
 
 	return smin;
 }
+// you can combine the above two here
+
+// another approach (no sorting still), combining both second largest and second
+// smallest isn't the concern
+std::pair<int, int> second_smallest_and_largest(const int* const arr, size_t sz) {
+	int min = INF, smin = INF;
+	int max = -INF, smax = -INF;
+	for (size_t i{}; i < sz; i++) {
+		min = std::min(min, arr[i]);
+		max = std::max(max, arr[i]);
+	}
+	for (size_t i{}; i < sz; i++) {
+		if (arr[i] < smin && min < arr[i]) {  // can also give min != arr[i]
+			smin = arr[i];
+		}
+		if (arr[i] > smax && max > arr[i]) {  // can also give max != arr[i]
+			smax = arr[i];
+		}
+	}
+
+	return std::make_pair(smin, smax);
+}
 
 int main(void) {
 	int arr[(size_t)INF] = { '\0' };
 	size_t count = 0;
 	while (std::cin >> arr[count++]) { }
-	std::cout << second_largest(arr, count - 1) << std::endl;
-	std::cout << second_smallest(arr, count - 1) << std::endl;
+	/* std::cout << second_largest(arr, count - 1) << std::endl;
+	std::cout << second_smallest(arr, count - 1) << std::endl; */
+
+	std::pair<int, int> res = second_smallest_and_largest(arr, count - 1);
+	std::cout << res.first << ", " << res.second << std::endl;
 	return 0;
 }
