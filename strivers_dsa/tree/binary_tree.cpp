e19422e5fcf0a44356177b0a2ee7e679cd749ccb
abhay <abhaysp95@protonmail.com>
2023-04-05 15:28:07 +0530

    feat: added pre, in, post and level order binary traversal

--- strivers_dsa/tree/binary_tree.cpp
+++ strivers_dsa/tree/binary_tree.cpp
@@ -37,9 +37,52 @@ void preorder_traversal_recursive(const Node* root) {
 	preorder_traversal_recursive(root->right);
 }
 
+void inorder_traversal_recursive(const Node* root) {
+	if (nullptr == root) {
+		return;
+	}
+
+	inorder_traversal_recursive(root->left);
+	cout << root->value << " ";
+	inorder_traversal_recursive(root->right);
+}
+
+void postorder_traversal_recursive(const Node* root) {
+	if (nullptr == root) {
+		return;
+	}
+
+	postorder_traversal_recursive(root->left);
+	postorder_traversal_recursive(root->right);
+	cout << root->value << " ";
+}
+
+void levelorder_traversal(const Node* root) {
+	queue<const Node*> q{};
+	if (nullptr == root) return;
+	q.push(root);
+
+	while (!q.empty()) {
+		const Node* my_node = q.front();
+		cout << my_node->value << " ";
+		q.pop();
+
+		if (nullptr != my_node->left) q.push(my_node->left);
+		if (nullptr != my_node->right) q.push(my_node->right);
+	}
+}
+
 int main(void) {
 	Node *root = nullptr;
 	add_few_node(root);
+
+	preorder_traversal_recursive(root);
+	cout << "\n----------------\n";
+	inorder_traversal_recursive(root);
+	cout << "\n----------------\n";
+	postorder_traversal_recursive(root);
+	cout << "\n----------------\n";
+	levelorder_traversal(root);
 
 	return 0;
 }
