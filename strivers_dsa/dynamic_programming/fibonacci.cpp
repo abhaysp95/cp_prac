3948337ef1abc2ff2e4615bb8e1f0308403dab6e
abhay <abhaysp95@protonmail.com>
2023-04-25 15:56:06 +0530

    feat: fibonacci memoization

--- strivers_dsa/dynamic_programming/fibonacci.cpp
+++ strivers_dsa/dynamic_programming/fibonacci.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstring>
 
 int fib_recursive(int n) {
 	if (0 == n || 1 == n) return n;
@@ -6,10 +7,29 @@ int fib_recursive(int n) {
 	return fib_recursive(n - 1) + fib_recursive(n - 2);
 }
 
+int arr[10001];
+
+int fib_memoization(int n) {
+	if (0 == n || 1 == n) {
+		return n;
+	}
+
+	if (arr[n] != -1) {
+		return arr[n];
+	}
+
+	arr[n] = fib_memoization(n - 1) + fib_memoization(n - 2);
+	return arr[n];
+}
+
 int main(void) {
 	int num = 0;
 	scanf("%d", &num);
 
-	printf("fib_recursive: %d\n", fib_recursive(num));
+	// printf("fib_recursive: %d\n", fib_recursive(num));
+
+	memset(arr, -1, sizeof(arr));
+	printf("fib_memoization: %d\n", fib_memoization(num));
+
 	return 0;
 }
