27dc4216120b4d0eceb3aa03427c379c053fb6cf
abhay <abhaysp95@protonmail.com>
2023-04-26 15:02:47 +0530

    feat: max_sum_of_non_adjacent_elements, tabular way

--- strivers_dsa/dynamic_programming/max_sum_of_non_adjacent_elements.cpp
+++ strivers_dsa/dynamic_programming/max_sum_of_non_adjacent_elements.cpp
@@ -34,13 +34,30 @@ int max_sum_non_adj_memoized(const vector<int>& arr, int idx, int *space) {
 	return space[idx];
 }
 
+int max_sum_non_adj_tabular(const vector<int>& arr) {
+	int space[arr.size() + 1];
+	memset(space, -1, sizeof(space));
+
+	space[0] = 0;
+	space[1] = arr[0];
+
+	for (size_t i = 2; i <= arr.size(); i++) {
+		space[i] = max(space[i - 1], space[i - 2] + arr[i - 1]);  // arr is moving 1 behind space
+	}
+
+	return space[arr.size()];
+}
+
 int main(void) {
 	vector<int> arr = {2, 1, 4, 9};
 
 	// printf("%d\n", max_sum_non_adj_recursive(arr, arr.size() - 1));
-	int space[arr.size()];
+
+	/* int space[arr.size()];
 	memset(space, -1, sizeof(space));
-	printf("%d\n", max_sum_non_adj_memoized(arr, arr.size() - 1, space));
+	printf("%d\n", max_sum_non_adj_memoized(arr, arr.size() - 1, space)); */
+
+	printf("%d\n", max_sum_non_adj_tabular(arr));
 
 	return 0;
 }
