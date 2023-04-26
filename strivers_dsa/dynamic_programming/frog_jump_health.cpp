af37aa963c951edbd0be42c1257a80e0e1182a3b
abhay <abhaysp95@protonmail.com>
2023-04-26 11:37:42 +0530

    feat: frog jump health, memoized way

--- strivers_dsa/dynamic_programming/frog_jump_health.cpp
+++ strivers_dsa/dynamic_programming/frog_jump_health.cpp
@@ -17,11 +17,31 @@ int frog_jump_health_recursive(const vector<int>& arr, int idx) {
 	return left;
 }
 
+// there is problem overlapping hence memoization of above problem can be done
+int frog_jump_health_memoized(const vector<int>& arr, int idx, int *space) {
+	if (idx == 0) {
+		return 0;
+	}
+
+	if (space[idx] != -1) {
+		return space[idx];
+	}
+	int left = frog_jump_health_memoized(arr, idx - 1, space) + abs(arr[idx] - arr[idx -1]);
+	if (idx > 1) {  // shouldn't jump two space if it's 1
+		int right = frog_jump_health_memoized(arr, idx - 2, space) + abs(arr[idx] - arr[idx - 2]);
+		return min(left, right);
+	}
+
+	return left;
+}
 
 int main(void) {
 	vector<int> health = {10, 20, 30, 10};
 
-	printf("%d\n", frog_jump_health_recursive(health, health.size() - 1));
+	// printf("%d\n", frog_jump_health_recursive(health, health.size() - 1));
+	int space[health.size()];
+	memset(space, -1, sizeof(space));
+	printf("%d\n", frog_jump_health_memoized(health, health.size() - 1, space));
 
 	return 0;
 }
