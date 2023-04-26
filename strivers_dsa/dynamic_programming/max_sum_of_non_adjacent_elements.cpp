f2f664ad2b72fc0bec7ebc97c51f04dd7a37d136
abhay <abhaysp95@protonmail.com>
2023-04-26 14:49:20 +0530

    feat: max_sum_of_non_adjacent_elements, memoized way

--- strivers_dsa/dynamic_programming/max_sum_of_non_adjacent_elements.cpp
+++ strivers_dsa/dynamic_programming/max_sum_of_non_adjacent_elements.cpp
@@ -17,10 +17,30 @@ int max_sum_non_adj_recursive(const vector<int>& arr, int idx) {
 	return max(no_pick, pick);
 }
 
+int max_sum_non_adj_memoized(const vector<int>& arr, int idx, int *space) {
+	if (idx == 0) {
+		return arr[idx];
+	}
+	if (idx < 0) {
+		return 0;
+	}
+
+	if (-1 != space[idx]) {
+		return space[idx];
+	}
+
+	space[idx] = max(max_sum_non_adj_memoized(arr, idx - 1, space),
+			arr[idx] + max_sum_non_adj_memoized(arr, idx - 2, space));
+	return space[idx];
+}
+
 int main(void) {
 	vector<int> arr = {2, 1, 4, 9};
 
-	printf("%d\n", max_sum_non_adj_recursive(arr, arr.size() - 1));
+	// printf("%d\n", max_sum_non_adj_recursive(arr, arr.size() - 1));
+	int space[arr.size()];
+	memset(space, -1, sizeof(space));
+	printf("%d\n", max_sum_non_adj_memoized(arr, arr.size() - 1, space));
 
 	return 0;
 }
