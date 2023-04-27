c2397a857a928b7514887d05e1c01123482c50ab
abhay <abhaysp95@protonmail.com>
2023-04-27 17:58:56 +0530

    feat: ninja training, non recursive way

--- strivers_dsa/dynamic_programming/ninja_training.cpp
+++ strivers_dsa/dynamic_programming/ninja_training.cpp
@@ -71,13 +71,34 @@ int maximum_training_merit_memoized(const vector<vector<int>>& training, int day
 	return maxi;
 }
 
+// this approach is not exactly based on the recursive solution I wrote above
+int maximum_training_merit_tabular(const vector<vector<int>>& training) {
+	int space[training.size()][3];
+	memset(space, 0, sizeof(space));
+
+	// fill up base day
+	for (int i = 0; i < 3; i++) {
+		space[0][i] = training[0][i];
+	}
+
+	for (int i = 1; i < training.size(); i++) {
+		space[i][0] = space[i - 1][0] + max(training[i][1], training[i][2]);
+		space[i][1] = space[i - 1][1] + max(training[i][0], training[i][2]);
+		space[i][2] = space[i - 1][2] + max(training[i][0], training[i][1]);
+	}
+
+	return space[training.size() - 1][2];
+}
+
 int main(void) {
-	vector<vector<int>> training ={ { 1, 2, 5 }, { 3, 1, 1 }, { 3, 3, 3 } };
+	vector<vector<int>> training = { {2, 1, 3}, {3, 4, 6}, {10, 1, 6}, {8, 3, 7} }; /* { { 1, 2, 5 }, { 3, 1, 1 }, { 3, 3, 3 } }; */
 
 	// printf("%d\n", maximum_training_merit_recursive(training, training.size() - 1, 3));
 
-	memset(space, -1, sizeof(int[3]) * training.size());
-	printf("%d\n", maximum_training_merit_memoized(training, training.size() - 1, 3));
+	/* memset(space, -1, sizeof(int[3]) * training.size());
+	printf("%d\n", maximum_training_merit_memoized(training, training.size() - 1, 3)); */
+
+	printf("%d\n", maximum_training_merit_tabular(training));
 
 	return 0;
 }
