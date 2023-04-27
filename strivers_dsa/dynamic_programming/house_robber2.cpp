b08f36a038a51b6fad45cd1b27e6a29e66589204
abhay <abhaysp95@protonmail.com>
2023-04-27 12:13:15 +0530

    feat: house robber 2, tabular way

--- strivers_dsa/dynamic_programming/house_robber2.cpp
+++ strivers_dsa/dynamic_programming/house_robber2.cpp
@@ -17,6 +17,30 @@ int house_robber_memoized(const vector<int>& arr, int start, int idx, int *space
 	return space[idx] = max(not_pick, pick);  // returns space[idx] after assignment
 }
 
+int house_robber_tabular(const vector<int>& nums) {
+	int space[nums.size()];  // cause we'll leave 1 house (in each case)
+
+	// leave the right-most house
+	space[0] = 0;
+	space[1] = nums[0];
+	for (size_t i = 2; i < nums.size(); i++) {
+		space[i] = max(space[i - 1], space[i - 2] + nums[i - 1]);
+	}
+	int left = space[nums.size() - 1];
+
+	// leave the left-most house
+	space[0] = 0;
+	space[1] = nums[1];
+	for (size_t i = 2; i < nums.size(); i++) {
+		space[i] = max(space[i - 1], space[i - 2] + nums[i]);
+	}
+	int right = space[nums.size() - 1];
+
+	printf("%d, %d\n", left, right);
+
+	return max(left, right);
+}
+
 // first house is adjacent of last house (circular structure)
 int house_robber_solution(const vector<int>& nums) {
 	if (nums.size() == 1) return nums[0];
@@ -27,15 +51,19 @@ int house_robber_solution(const vector<int>& nums) {
 	memset(space, -1, sizeof(space));
 	int right = house_robber_memoized(nums, 1, nums.size(), space);  // leaving first house
 
-	// printf("%d, %d", left, right);
-
 	return max(left, right);
 }
+
+// NOTE: the easiest thing to do would be to make 2 different arrays, leaving
+// one house from each end in each of the arrays respectively and then use the
+// same algo we used in "max sum of non adjacent" algo. I used the same algo
+// here, but I didn't made two different arrays, which made it slightly
+// difficult
 
 int main(void) {
 	vector<int> vec = { 200, 3, 140, 20, 10 }; /* { 1, 2, 3, 1 }; */ /* { 2, 3, 2 }; */
 
-	printf("%d\n", house_robber_solution(vec));
+	printf("%d\n", house_robber_tabular(vec));
 
 	return 0;
 }
