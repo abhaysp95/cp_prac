a9828aaca746880dac348fb36c4a017729b350b6
abhay <abhaysp95@protonmail.com>
2023-05-02 21:48:35 +0530

    feat: some basic number theory ques. essential for cp

--- efficient_coding/l1/basic_number_theory.cpp
+++ efficient_coding/l1/basic_number_theory.cpp
@@ -10,8 +10,7 @@ using namespace std;
 
 const int MODULO = 1e9 + 7;  // first 10-digit prime and fits in the range of int data type
 
-int main() {
-	// find product of these numbers
+void big_number_product() {
 	int arr[] = {1766342282, 874738772, 892884722};
 	int len = sizeof(arr) / sizeof(int);
 
@@ -21,4 +20,65 @@ int main() {
 	}
 
 	cout << product << endl;
+}
+
+int calculate_power_recursive(int n, int b) {
+	if (b == 0) return 1;
+	if (b % 2 == 0) {
+		return calculate_power_recursive(n * n, b / 2);
+	} else {
+		return n * calculate_power_recursive(n * n, (b - 1) / 2);
+	}
+}
+
+int modular_exponentiation_recursive(int n, int b, int c) {
+	if (b == 0) return 1;
+	if (b % 2 == 0) {
+		return modular_exponentiation_recursive((n * n) % c, b / 2, c);
+	} else {
+		return (n * modular_exponentiation_recursive((n * n) % c, b / 2, c)) % c;
+	}
+}
+
+int modular_exponentiation_iteration(int n, int b, int c) {
+	int result = 1;
+	while (b != 0) {
+		/* if (b % 2 == 0) {
+			n = (n * n) % c;
+		} else {
+			result = (result * n) % c;
+			n = (n * n) % c;
+		}
+		b /= 2; */
+		if (b % 2 == 1) {
+			result = (result * n) % c;
+		}
+		n = (n * n) % c;
+		b = b / 2;
+	}
+
+	return result;
+}
+
+int gcd(int first, int second) {
+	// base condition
+	if (first % second == 0) {
+		return second;
+	}
+	int third = first % second;
+	return gcd(second, third);
+}
+
+int gcd_iterative(int first, int second) {
+	while (first % second != 0) {
+		int third = first % second;
+		first = second;
+		second = third;
+	}
+
+	return second;
+}
+
+int main() {
+	cout << gcd(12, 20) << endl;
 }
