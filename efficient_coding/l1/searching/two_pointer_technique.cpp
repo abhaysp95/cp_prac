e6ed42b0254ebce0abd400c976e142cda2df3d70
abhay <abhaysp95@protonmail.com>
2023-05-03 18:20:49 +0530

    feat: remove duplicate & update array, two pointer technique

--- efficient_coding/l1/searching/two_pointer_technique.cpp
+++ efficient_coding/l1/searching/two_pointer_technique.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstdio>
 
 using namespace std;
 
@@ -22,6 +23,58 @@ void two_sum(void) {
 	printf("two sum: (%d, %d)\n", x, y);
 }
 
+// update the array by removing duplicates and also give the new length
+void remove_duplicates(void) {
+	vector<int> nums = {1, 1, 2, 2, 2, 3, 3};
+
+	if (nums.empty() || nums.size() == 1) {
+		// return this lenght
+		printf("updated len: %zu\n", nums.size());
+		return;
+	}
+
+	size_t i{1}, j{1};
+	for (; i < nums.size(); i++) {
+		// if duplicate found
+		if (i != j && nums[i] != nums[j]) {
+			if (nums[i] == nums[i - 1]) {
+				nums[j] = nums[i];
+				j++;
+			}
+		}
+	}
+
+	printf("updated len: %zu\n", j);
+
+	// print this new array
+	for (size_t x{}; x < j; x++) {
+		printf("%d ", nums[x]);
+	}
+	puts("\n");
+}
+
+// better in coding only, complexity is similar
+void remove_duplicates_better(void) {
+	vector<int> nums = {1, 1, 2, 2, 2, 3, 3};
+
+	size_t i = 1, j = 0;
+	for (; i < nums.size(); i++) {
+		if (nums[i] != nums[j]) {  // unique
+			nums[++j] = nums[i];
+		}
+	}
+	j++; // shows new size
+
+	printf("updated len: %zu\n", j);
+
+	// print this new array
+	for (size_t x{}; x < j; x++) {
+		printf("%d ", nums[x]);
+	}
+	puts("\n");
+}
+
 int main(void) {
 	two_sum();
+	remove_duplicates_better();
 }
