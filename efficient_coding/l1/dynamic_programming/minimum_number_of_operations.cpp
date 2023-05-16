caa2d6cdf3887a62484b49442ea624c7c3065afe
abhay <abhaysp95@protonmail.com>
2023-05-16 18:13:38 +0530

    feat: minimum number of operations, tabulation method

--- efficient_coding/l1/dynamic_programming/minimum_number_of_operations.cpp
+++ efficient_coding/l1/dynamic_programming/minimum_number_of_operations.cpp
@@ -22,12 +22,29 @@ size_t minimum_operations(size_t num, vec& space) {
 	return space[num] = min(res1, res2) + 1;
 }
 
+size_t minimum_operations_tabulation(size_t num) {
+	vec space(num + 1, 0);
+
+	for (size_t i = 1; i <= num; i++) {
+		size_t res1{INT_MAX}, res2{INT_MAX};
+		if (i % 2 == 0) {
+			res1 = space[i / 2];
+		}
+		res2 = space[i - 1];
+		space[i] = 1 + min(res1, res2);
+	}
+
+	return space[num];
+}
+
 int main(void) {
 	size_t num{};
 	cin >> num;
 
-	vec space(num + 1, -1);
-	cout << minimum_operations(num, space);
+	/* vec space(num + 1, -1);
+	cout << minimum_operations(num, space) << endl; */
+
+	cout << minimum_operations_tabulation(num) << endl;
 
 	return 0;
 }
