dd49e3347c8d99465a4521f193e792114e05b787
abhay <abhaysp95@protonmail.com>
2023-05-23 10:43:40 +0530

    feat: two sum, input arr is sorted

---
+++ neetcode/two_pointer/two_sum_ii_input_array_is_sorted.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int start = 0, end = numbers.size() - 1;
+
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return vector<int>{start + 1, end + 1};
+            } else if (numbers[start] + numbers[end] < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return vector<int>{};
+    }
+};
