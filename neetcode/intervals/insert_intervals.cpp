c036a6945174f954cfc2d647f1d07a81f768bd43
abhay <abhaysp95@protonmail.com>
2023-06-13 22:13:22 +0530

    feat: insert interval into list of non-overlapping way, such that after insertion there's no overlap

---
+++ neetcode/intervals/insert_intervals.cpp
@@ -0,0 +1,69 @@
+/* You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+
+
+Constraints:
+
+    0 <= intervals.length <= 104
+    intervals[i].length == 2
+    0 <= starti <= endi <= 105
+    intervals is sorted by starti in ascending order.
+    newInterval.length == 2
+    0 <= start <= end <= 105 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using vvec = vector<vector<int>>;
+using vec = vector<int>;
+
+class Solution {
+	public:
+		vvec insert(vvec& intervals, vec newInterval) {
+			vvec res{};
+
+			// already sorted intervals
+			size_t i = 0;
+			bool added = false;
+			while (i < intervals.size()) {
+				// non-overlapping
+				if (newInterval[1] < intervals[i][0]) {
+					res.push_back(newInterval);
+					added = true;
+					break;
+				}
+				if (newInterval[0] > intervals[i][1]) {
+					res.push_back(intervals[i]);
+				} else {  // overlapping
+					newInterval[0] = max(newInterval[0], intervals[i][0]);
+					newInterval[1] = max(newInterval[1], intervals[i][1]);
+				}
+				i++;
+			}
+			while (i < intervals.size()) {
+				res.push_back(intervals[i]);
+			}
+			if (!added) {
+				res.push_back(newInterval);
+			}
+
+			return res;
+		}
+};
